a6044390c2193a3e5085668edc0449afc8c5ed7e
jossyvincent <vincentjossy0@gmail.com>
2025-09-05 23:23:29 +0300

    Finished navigation bar

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,23 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Menu, X } from "lucide-react";
+
+import { useState } from 'react';
 
 const LandingPage = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <main className='min-h-screen '>
         <nav className='w-full fixed top-0 left-0 z-50'>
             <div className='max-w-xl mx-auto px-4 py-3 items-center flex justify-between'>
-                <div className='text-dominant font-black text-lg'>Vincent Jossy</div>
+                {/* Name or Logo */}
+                <div className='text-dominant font-black text-lg'>Vincent Jossy
+                </div>
+                {/*Desktop menu */}
                 <ul className='hidden md:flex space-x-8 text-sm font-medium'>
                     <li>
-                        <a href='#home'>Home</a>
+                        <a href='#home'className='hover:text-dominant transition'>Home</a>
                     </li>
                     <li>
-                        <a href='#about'>About</a>
+                        <a href='#about' className='hover:text-dominant transition'>About</a>
                     </li>
                     <li>
-                        <a href='#home'>Projects</a>
+                        <a href='#home' className='hover:text-dominant transition'>Projects</a>
                     </li>
                 </ul>
+
+                {/* Mobile menu */}
+                <button className='md:hidden bg-white text-dominant' onClick={()=>setIsOpen(!isOpen)}>
+                    {isOpen ? <X size={24}/> : <Menu size={24} />}
+
+                </button>
+            
+            {/* Mobile menu drop-down */}
+                {isOpen && (
+                    <div className='md:hidden bg-white border-t shadow-xl'>
+                        <ul className='flex flex-col items-center space-y-4 py-4 text-sm font-medium'>
+                            <li>
+                                <a href="#home" className='hover:text-dominant transition'
+                                 onClick={()=> setIsOpen(false)}>Home</a>
+                            </li>
+                            <li>
+                                <a href="#About" className='hover:text-dominant transition'
+                                 onClick={()=> setIsOpen(false)} >About</a>
+                            </li>
+                            <li>
+                                <a href="#projects" className='hover:text-dominant transition'
+                                 onClick={()=> setIsOpen(false)}>Projects</a>
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </div>
+            
         </nav>
 
     </main>
